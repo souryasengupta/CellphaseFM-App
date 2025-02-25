306e0ab5ca10b857b9b710b639518799ef52cbce
Sourya Sengupta <44081132+souryasengupta@users.noreply.github.com>
2025-02-24 20:39:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -129,5 +129,8 @@ def index():
 def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
+import os
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
